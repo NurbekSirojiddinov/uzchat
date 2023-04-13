4de475161c82d9cdf7c2baa81d246de6afc753ab
Nurbek Sirojiddinov <nurbek20020307@gmail.com>
2023-04-14 00:12:36 +0500

    Added some fields to User

--- src/main/java/com/developers/uzchat/domain/User.java
+++ src/main/java/com/developers/uzchat/domain/User.java
@@ -1,12 +1,19 @@
 package com.developers.uzchat.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String name;
+
+    private String username;
 
     public void setId(Long id) {
         this.id = id;
